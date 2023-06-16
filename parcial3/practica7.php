7ae11cab44bd685171475f6eedc7d4137228a0a8
Lopez Julian <124410770+julian200606@users.noreply.github.com>
2023-06-15 19:00:30 -0700

    Add files via upload

--- parcial3/practica7.php
+++ parcial3/practica7.php
@@ -4,48 +4,52 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Serpientes y escaleras</title>
-    <link rel="stylesheet" href="bootstrap.css">
+    <title>Practica 7</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
     <style>
         .casilla{
             height: 50px;
-            margin: 3px;
+            font-weight:bold;
         }
         .escalera{
-            background-image: url('escalera.jpg');
-            background-size: cover;           
-             margin: 3px;
+            background-image: url('Escalerilla.png');
+            background-size: cover;
         }
         .serpiente{
-            background-image: url('Serpiente.png');
-            background-size: cover;
+            background-image: url('serpiente.png');
+            background-size: contain;
             background-repeat: no-repeat;
-            margin: 3px;
+            background-position:center;
         }
     </style>
 </head>
 <body>
     <?php
-        $colores=["#C39BD3", "#A9CCE3", "#D4EFDF","#FCF3CF", "#F5B7B1","#B0C4DE"]
+        $colores = ["#a62ba2", "#1480de", "#9ee344", "#033845", "#bf452c", "#6908a1", "#b50954"];
+        
+                  
     ?>
     <div class="container">
-        <h1>Práctica 7 - Serpientes y escaleras</h1><hr>
-        <form action="">
-            <div class="row">
-                <?php
+         <h1>Tablero de serpientes y escaleras</h1><hr>
+            <form action="practica7.php" method="POST"> 
+                <input type="submit" value="jugar" class="btn btn-primary"><hr>
+                <div class="row">
+                    <?php
                     for($i=100; $i>0; $i--){
-                        if($i==4 ||$i==9||$i==19||$i==28||$i==40||$i==63||$i==80||$i==75){
-                            echo "<div class='col-1 card escalera' style='background-color:".$colores[rand(0,5)].";'>".$i."</div>";
-
-                        }elseif($i==17 ||$i==34||$i==99||$i==64||$i==95||$i==93||$i==87||$i==62){
-                            echo "<div class='col-1 card serpiente' style='background-color:".$colores[rand(0,5)].";'>".$i."</div>";
-                        }else{
-                            echo "<div class='col-1 card casilla' style='background-color:".$colores[rand(0,5)].";'>".$i."</div>";                           
-                        }
+                        if($i==4||$i==9||$i==20||$i==28||$i==40||$i==63||$i==71){
+                       echo"<div class='col-1 card m-1 casilla escalera' style='background-color:".$colores[rand(0,6)].";'>".$i."</div>";
+                    }else if($i==17||$i==54||$i==62||$i==64||$i==87||$i==93||$i==95){
+                        echo"<div class='col-1 card m-1 casilla serpiente' style='background-color:".$colores[rand(0,6)].";'>".$i."</div>";
+                    }else{
+                        echo"<div class='col-1 card m-1 casilla' style='background-color:".$colores[rand(0,6)].";'>".$i."</div>";
                     }
-                ?>
-            </div>
-        </form>
+                }
+                    ?>
+                </div>
+            </form>
+
     </div>
+   
+  
 </body>
 </html>
